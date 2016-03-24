aafc0b11265c4ce16c3172b71fc3af223e6236d2
Leonardo Uieda <leouieda@gmail.com>
2016-03-24 08:24:55 -0300

    Convert text in EPS figures to paths

--- manuscript/figures/strip-fonts.sh
+++ manuscript/figures/strip-fonts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Inspired by the solutions in 
+# Remove the fonts embedded in the EPS files. The journal doesn't like them.
+# Inspired by the solutions in
 # http://stackoverflow.com/questions/28797418/replace-all-font-glyphs-in-a-pdf-by-converting-them-to-outline-shapes
 # http://tex.stackexchange.com/questions/24355/convert-from-pdf-to-eps-while-keeping-outlining-fonts
 
@@ -8,8 +9,7 @@ for f in *.eps
 do
     epstool --copy --bbox $f tmp.eps
     gs -dNOPAUSE -dNOCACHE -dBATCH -sDEVICE=pdfwrite -dNoOutputFonts -dEPSCrop \
-        -sOutputFile="$(basename "$f" .eps).pdf" tmp.eps    
-    pdftops "$(basename "$f" .eps).pdf" -eps $f
-    convert -density 300 $f "$(basename "$f" .eps).png"
-    rm tmp.eps "$(basename "$f" .eps).pdf"
+        -sOutputFile=tmp.pdf tmp.eps
+    pdftops tmp.pdf -eps $f
+    rm tmp.eps tmp.pdf
 done
