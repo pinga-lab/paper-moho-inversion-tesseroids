654bc6555b172c61f1326f668c6cb55cdeef1d18
Leonardo Uieda <leouieda@gmail.com>
2016-08-17 10:44:57 -0300

    Make png conversion better use the Makefile
    Instead of converting all eps to png in the script, use the Makefile to
    manage dependencies.

--- manuscript/figures/topng.sh
+++ manuscript/figures/topng.sh
@@ -3,16 +3,12 @@
 # Convert the EPS files to PNG for the initial submission. The EPS are too high
 # res (especially the Moho models) and don't render properly on the journal
 # system.
-# use: bash topng.sh DPI
-
-for f in *.eps
-do
-    echo "Converting "$f
-    # Convert first to highres PNG because converting the Moho estimates to
-    # lowres is ugly. White lines appear between the model tiles.
-    convert -density 1200 $f tmp.png
-    # Now I can resample the highres PNG to a lower DPI without the ugly white
-    # lines.
-    convert -units PixelsPerInch tmp.png -resample 300 "$(basename "$f" .eps).png"
-    rm tmp.png
-done
+# use: bash topng.sh figure.eps
+OUT=${1%.eps}.png
+echo "Converting "$1 to $OUT
+# Convert first to highres PNG because converting the Moho estimates to
+# lowres is ugly. White lines appear between the model tiles.
+convert -density 1200 $1 $OUT
+# Now I can resample the highres PNG to a lower DPI without the ugly white
+# lines.
+convert -units PixelsPerInch $OUT -resample 300 $OUT
